e4b2cbfdb715fc79e420904f44cf9f511339ecc5
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2021-11-29 00:11:10 +0600

    add env file

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
+
+dotenv.config()
 
 const app = express()
 
@@ -12,8 +15,13 @@ app.get('/api/products', (req, res) => {
 })
 
 app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-  })
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
+})
 
-app.listen(2000, console.log('Server running on port 2000'))
+const PORT = process.env.PORT || 2000
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
