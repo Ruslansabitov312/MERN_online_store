2e3ebfaac7ba8f784eafa8c4f502a1e2fe706841
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2021-12-13 19:06:53 +0600

    Admin user list

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -11,12 +11,13 @@ import {
   userRegisterReducer,
   userDetailsReducer,
   userUpdateProfileReducer,
+  userListReducer,
 } from './reducers/userReducers'
 import {
   orderCreateReducer,
   orderDetailsReducer,
   orderPayReducer,
-  orderListMyReducer
+  orderListMyReducer,
 } from './reducers/orderReducers'
 
 const reducer = combineReducers({
@@ -27,6 +28,7 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  userList: userListReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
