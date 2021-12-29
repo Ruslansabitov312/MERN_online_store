78c135c0f700b0c9eb6d9c7cb554270fe6bea088
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2021-12-30 01:03:05 +0600

    Frontend product reviews

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,6 +7,7 @@ import {
   productDeleteReducer,
   productCreateReducer,
   productUpdateReducer,
+  productReviewCreateReducer,
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import {
@@ -31,6 +32,7 @@ const reducer = combineReducers({
   productDelete: productDeleteReducer,
   productCreate: productCreateReducer,
   productUpdate: productUpdateReducer,
+  productReviewCreate: productReviewCreateReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
