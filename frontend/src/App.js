cf4c9e064b4ecb6d1fb421acba33fb814a41bd2e
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2022-01-02 00:55:10 +0600

    Product Search Box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,6 +38,7 @@ const App = () => {
           <Route path='/admin/productlist' component={ProductListScreen} />
           <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
           <Route path='/' component={HomeScreen} exact />
+          <Route path='/search/:keyword' component={HomeScreen} />
         </Container>
       </main>
       <Footer />
